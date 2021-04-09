fe6ec232c7840069c523bc7fd498e31956568f99
NurvKPuSseegeova <zhuangshoupu561622@126.com>
2021-04-09 22:53:17 +0800

    添加MAP

--- src/payroll/PayrollDatabase.java
+++ src/payroll/PayrollDatabase.java
@@ -1,10 +1,15 @@
 package payroll;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class PayrollDatabase {
+	
+	private static Map<Integer, Employee> employees = new LinkedHashMap<>();
 
 	public static Employee getEmployee(int empId) {
 		// TODO Auto-generated method stub
-		return null;
+		return employees.get(empId);
 	}
 
 }
