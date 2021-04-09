ee9abceb47360798a596d622b5e24b3cc5f60d8e
NurvKPuSseegeova <zhuangshoupu561622@126.com>
2021-04-09 22:36:07 +0800

    创建execute

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -4,8 +4,23 @@ import payroll.Transaction;
 
 public class AddHourlyEmployeeTransaction implements Transaction {
 
+	private int empId;
+	private String name;
+	private String address;
+	private double hourlyRate;
+
 	public AddHourlyEmployeeTransaction(int empId, String name, String address, double hourlyRate) {
+		this.empId = empId;
+		this.name = name;
+		this.address = address;
+		this.hourlyRate = hourlyRate;
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
