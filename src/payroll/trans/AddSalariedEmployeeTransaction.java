8766568b27df39bb43bc1cf8147d40a67a4333a6
NurvKPuSseegeova <zhuangshoupu561622@126.com>
2021-04-10 15:18:11 +0800

    实现getSalary方法

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -1,6 +1,10 @@
 package payroll.trans;
 
+import payroll.Employee;
+import payroll.PayrollDatabase;
 import payroll.Transaction;
+import payroll.clasification.SalariedClassification;
+import payroll.method.HoldMethod;
 
 public class AddSalariedEmployeeTransaction implements Transaction {
 
@@ -19,7 +23,14 @@ public class AddSalariedEmployeeTransaction implements Transaction {
 
 	@Override
 	public void execute() {
-		//
+		//新建雇员
+		Employee employee = new Employee(empId,name,address);
+		//设置工资计算方式
+		employee.setPaymentClassification(new SalariedClassification(salary));
+		//设置工资支付方法
+		employee.setPaymentMethod(new HoldMethod());
+		//保存到数据库
+		PayrollDatabase.save(employee);
 
 	}
 
