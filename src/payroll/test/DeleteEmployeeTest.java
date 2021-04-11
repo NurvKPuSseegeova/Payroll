ead4b00e995c682592f611887b45f9716bd022cd
NurvKPuSseegeova <zhuangshoupu561622@126.com>
2021-04-11 11:10:22 +0800

    删除雇员，验证雇员已删除

--- src/payroll/test/DeleteEmployeeTest.java
+++ src/payroll/test/DeleteEmployeeTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import payroll.Employee;
+import payroll.PayrollDatabase;
 import payroll.Transaction;
 import payroll.trans.AddHourlyEmployeeTransaction;
 import payroll.trans.DeleteEmployeeTransaction;
